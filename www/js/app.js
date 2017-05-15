51a108e2795656aa34b1c10fecd62d353f05f21a
dcatalan <d.catalan@apploading.com>
2017-05-15 16:21:12 +0200

    mas controladores ligados a todos los modulos del proyecto

--- www/js/app.js
+++ www/js/app.js
@@ -4,154 +4,157 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'ionic-material', 'starter.controllers', 'ngMaterial'])
+var app = angular.module('starter', ['ionic', 'ionic-material', 'starter.controllers', 'ngMaterial'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider, $httpProvider, $provide, $ionicConfigProvider) {
-
-    $ionicConfigProvider.views.maxCache(0);
-
-
-  $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl',
-  })
-
-  .state('app.contact', {
-      url: '/contact',
-      views: {
-          'menuContent': {
-              templateUrl: 'templates/contacto.html'
-          }
-      }
-  })
-
-  .state('app.products', {
-      url: '/products',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/productos.html'
-        }
-      }
-    })
-    .state('app.micarrito', {
-        url: '/micarrito',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/micarrito.html',
-                controller: 'misCarrosCtrl'
             }
-        }
-    })
-    .state('app.seguimientos', {
-        url: '/seguimientos',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/seguimientos.html',
-                controller: 'seguimientoCtrl'
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
             }
+        });
+    })
+
+    .config(function ($stateProvider, $urlRouterProvider, $httpProvider, $provide, $ionicConfigProvider) {
+
+        $ionicConfigProvider.views.maxCache(0);
+
+
+        $stateProvider
+
+            .state('app', {
+                url: '/app',
+                abstract: true,
+                templateUrl: 'templates/menu.html',
+                controller: 'AppCtrl'
+            })
+
+            .state('app.contact', {
+                url: '/contact',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'modules/contact/contacto.html',
+                        controler: 'contactCtrl'
+                    }
+                }
+            })
+
+            .state('app.products', {
+                url: '/products',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'modules/productSearcher/productos.html',
+                        controler: 'productSearcherCtrl'
+                    }
+                }
+            })
+            .state('app.micarrito', {
+                url: '/micarrito',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'modules/myChart/micarrito.html',
+                        controller: 'misCarrosCtrl'
+                    }
+                }
+            })
+            .state('app.seguimientos', {
+                url: '/seguimientos',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'modules/follow/seguimientos.html',
+                        controller: 'seguimientoCtrl'
+                    }
+                }
+            })
+            .state('app.scanner', {
+                url: '/scanner',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'modules/scanner/scanner.html',
+                        controler: 'scannerCtrl'
+                    }
+                }
+            })
+            .state('app.productosCarro', {
+                url: '/productosCarro',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'modules/productsChart/productosCarro.html',
+                        controller: 'productosCarroCtrl'
+                    }
+                }
+            })
+            .state('app.productos', {
+                url: '/productos',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'modules/productSearcher/productos.html',
+                        controller: 'productosCtrl'
+                    }
+                }
+            })
+            .state('login', {
+                url: '/login',
+                controller: 'loginCtrl',
+                templateUrl: 'modules/login/login.html'
+
+            }).state('registro', {
+            url: '/registro',
+            controller: 'registroCtrl',
+            templateUrl: 'modules/register/registro.html'
+
+        });
+
+        $provide.factory('AuthInterceptor', function ($q, $location) {
+
+            return {
+                // On request success
+                request: function (config) {
+
+
+
+                    // console.log(config); // Contains the data about the request before it is sent.
+                    if (($location.path() === "/login" || $location.path() === "/registro") && localStorage.getItem("access_token")) {
+
+                        $location.path("/app/micarrito");
+
+                    } else if (!localStorage.getItem("access_token") && $location.path() != "/registro") {
+                        $location.path("/login");
+
+                    } else if (localStorage.getItem("access_token")) {
+
+                    }
+
+
+                    return config || $q.when(config);
+                    // Return the config or wrap it in a promise if blank.
+
+                },
+                // On response success
+                response: function (response) {
+                    // console.log(response); // Contains the data from the response.
+                    // Return the response or promise.
+
+                    return response || $q.when(response);
+                }
+            };
+        });
+
+        $httpProvider.interceptors.push('AuthInterceptor');
+
+        if (localStorage.getItem("access_token")) {
+            $urlRouterProvider.otherwise('/app/micarrito');
+        } else {
+            $urlRouterProvider.otherwise('/login');
         }
-    })
-    .state('app.scanner', {
-        url: '/scanner',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/scanner.html'
-            }
-        }
-    })
-    .state('app.productosCarro', {
-        url: '/productosCarro',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/productosCarro.html',
-                controller: 'productosCarroCtrl'
-            }
-        }
-    })
-    .state('app.productos',{
-        url: '/productos',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/productos.html',
-                controller: 'productosCtrl'
-            }
-        }
-    })
-    .state('login', {
-        url: '/login',
-        controller: 'loginCtrl',
-        templateUrl: 'templates/login.html'
-
-    }).state('registro', {
-        url: '/registro',
-        controller: 'registroCtrl',
-        templateUrl: 'templates/registro.html'
-
-    })
-
-    $provide.factory('AuthInterceptor', function ($q, $location) {
-
-              return {
-                  // On request success
-                  request: function (config) {
 
 
-
-                      // console.log(config); // Contains the data about the request before it is sent.
-                      if (($location.path() === "/login" || $location.path() === "/registro") && localStorage.getItem("access_token")) {
-
-                          $location.path("/app/micarrito");
-
-                      } else if (!localStorage.getItem("access_token") && $location.path() != "/registro") {
-                          $location.path("/login");
-
-                      }else if(localStorage.getItem("access_token")){
-
-                      }
-
-
-                      return config || $q.when(config);
-                      // Return the config or wrap it in a promise if blank.
-
-                  },
-                  // On response success
-                  response: function (response) {
-                      // console.log(response); // Contains the data from the response.
-                      // Return the response or promise.
-
-                      return response || $q.when(response);
-                  }
-              };
-          });
-
-          $httpProvider.interceptors.push('AuthInterceptor');
-
-          if (localStorage.getItem("access_token")) {
-              $urlRouterProvider.otherwise('/app/micarrito');
-          } else {
-              $urlRouterProvider.otherwise('/login');
-          }
-
-
-  // if none of the above states are matched, use this as the fallback
-});
+        // if none of the above states are matched, use this as the fallback
+    });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,244 +1,46 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout,$location,$http) {
-
-  console.log("appCargado");
+    .controller('AppCtrl', function ($scope, $ionicModal, $timeout, $location, $http) {
 
 
+        $scope.dominio = "http://localhost:3005";
 
-  $scope.dominio = "http://localhost:3005";
+        $scope.closeSession = function () {
+            localStorage.removeItem("access_token");
+            localStorage.removeItem("refresh_token");
+            $scope.usuario = {};
+            $location.path('/login');
+        };
 
-  $scope.closeSession = function () {
-        localStorage.removeItem("access_token");
-        localStorage.removeItem("refresh_token");
-        $scope.usuario = {};
-        $location.path('/login');
-    };
+        $scope.token = {};
+        $scope.token.accesToken = localStorage.getItem("access_token");
+        $scope.token.refreshToken = localStorage.getItem("refresh_token")
 
-    $scope.token ={}
-    $scope.token.accesToken = localStorage.getItem("access_token");
-    $scope.token.refreshToken = localStorage.getItem("refresh_token")
+        var ajax;
 
-    var ajax;
-
-    $scope.verificarToken = function(){
-    console.log("verificando");
-      ajax = $http.post($scope.dominio + '/token/validarToken', $scope.token );
-      ajax.success(function (data, status, headers, config) {
-          if(data === 500){
-            $scope.closeSession();
-          }else if(data === 502){
-            $scope.refrescarToken();
-          }
-      });
-    }
-
-    $scope.refrescarToken = function(){
-      ajax = $http.post($scope.dominio + '/token/refrescarToken', $scope.token );
-      ajax.success(function (data, status, headers, config) {
-          if(data){
-            localStorage.setItem("access_token", data.accesToken);
-            localStorage.setItem("refresh_token", data.refreshToken);
-          }else{
-            $scope.closeSession();
-          }
-      });
-    }
-
-
-    $scope.verificarToken();
-
-
-}).controller("registroCtrl", function($http, $scope,$location){
-
-  $scope.register = {};
-
-  $scope.dominio = "http://localhost:3005";
-
-    var obtenerLocalidadesRegistro = function(){
-      console.log($scope.dominio);
-    var ajax =  $http.get($scope.dominio + "/ObtenerTodasLocalidades");
-    ajax.success(function(data, status, headers, config){
-      $scope.localidades = data;
-    });
-};
-    obtenerLocalidadesRegistro();
-
-  $scope.doRegistro = function(){
-    var ajax =  $http.post($scope.dominio + "/usuario/insertarUsuario",$scope.register);
-    ajax.success(function(data, status, headers, config){
-        $location.path("/login");
-    });
-  };
-
-
-
-})
-.controller("loginCtrl", function($http, $scope,$ionicModal,$location){
-
-  $scope.dominio = "http://localhost:3005";
-
-    $scope.loginData = {};
-
-    var createToken = function () {
-              var res = $http.post($scope.dominio + '/token/crearToken', $scope.loginData);
-              res.success(function (data, status, headers, config) {
-                console.log(data);
-                if(data.accesToken && data.refreshToken){
-                  localStorage.setItem("access_token", data.accesToken);
-                  localStorage.setItem("refresh_token", data.refreshToken);
-                  $location.path("/app/micarrito");
+        $scope.verificarToken = function () {
+            console.log("verificando");
+            ajax = $http.post($scope.dominio + '/token/validarToken', $scope.token);
+            ajax.success(function (data, status, headers, config) {
+                if (data === 500) {
+                    $scope.closeSession();
+                } else if (data === 502) {
+                    $scope.refrescarToken();
                 }
-              });
-          };
+            });
+        };
 
-        $scope.login = function(){
-              createToken();
-          }
-}).factory('misCarrosService',function(){
+        $scope.refrescarToken = function () {
+            ajax = $http.post($scope.dominio + '/token/refrescarToken', $scope.token);
+            ajax.success(function (data, status, headers, config) {
+                if (data) {
+                    localStorage.setItem("access_token", data.accesToken);
+                    localStorage.setItem("refresh_token", data.refreshToken);
+                } else {
+                    $scope.closeSession();
+                }
+            });
+        };
 
-  var id_carro = 1;
-  var precio = 0;
-
-  return{
-    id_carro:id_carro,
-    getData:function(){
-      return precio;
-    },
-    setData: function(dinero) {
-        precio = dinero;
-      }
-  }
-
-}).controller("misCarrosCtrl", function($http, $scope,$ionicModal,$location,misCarrosService){
-  $scope.verificarToken();
-  $scope.carros = {};
-
-
-  var carros = $http.post($scope.dominio + '/usario/carros/obtenerCarrosUsuario',$scope.token);
-  carros.success(function (data, status, headers, config) {
-      $scope.carros = data;
-      $scope.obtenerPrecioCarros();
-
-  });
-
-
-
-  var dinero = 0;
-  var indexCarro = 0;
-  $scope.obtenerPrecioCarros = function(){
-    for(var i = 0; i < $scope.carros.length;i++){
-      if($scope.carros[i].productos.length === 0){
-        $scope.carros[i].precioCarro = 0;
-      }else{
-        $scope.carros[indexCarro].precioCarro = 0;
-      for(var j = 0; j < $scope.carros[i].productos.length;j++){
-        var res = $http.get($scope.dominio + '/productoTienda/obtenerProductoTiendaPorId?id='+$scope.carros[i].productos[j].idProductoTienda);
-        res.success(function (data, status, headers, config) {
-          if(data.historialPrecio.length > 0){
-            console.log("index:" +data.historialPrecio[data.historialPrecio.length-1].precio);
-            $scope.carros[indexCarro].precioCarro = $scope.carros[indexCarro].precioCarro + data.historialPrecio[data.historialPrecio.length-1].precio;
-            console.log(  $scope.carros[indexCarro].precioCarro);
-          }
-        });
-      }
-
-    }
-
-    }
-
-  }
-
-
-    $scope.click = function(id_carro){
-      misCarrosService.id_carro = id_carro;
-      $location.path("/app/productosCarro");
-    }
-
-}).controller("productosCarroCtrl", function($http, $scope,$ionicModal,$location,misCarrosService){
-  $scope.verificarToken();
-
-  $scope.productosCarro = {};
-
-  var dataProductosCarro = {
-    "accesToken":  localStorage.getItem("access_token"),
-    "id_carro": misCarrosService.id_carro
-  }
-
-  $scope.obtenerProductosCarro = function(){
-      $scope.verificarToken();
-    var res = $http.post($scope.dominio + '/carro/obtenerProductosCarro', dataProductosCarro);
-    res.success(function (data, status, headers, config) {
-      $scope.productosCarro = data;
-      console.log($scope.productosCarro);
-      //$scope.productosCarro.precio
+        $scope.verificarToken();
     });
-  }
-
-  $scope.obtenerProductosCarro();
-  console.log($scope.productosCarro);
-
-}).controller("productosCtrl", function($http, $scope,$ionicModal,$location){
-  $scope.verificarToken();
-console.log("dentro de productosCtrl");
-  $scope.productos = {}
-
-  $scope.dominio = "http://localhost:3005";
-
-  $scope.obtenerProductosCarro = function(){
-      $scope.verificarToken();
-    var res = $http.get($scope.dominio + '/producto/obtenerTodosProductos');
-    res.success(function (data, status, headers, config) {
-      console.log("productos:"+data);
-      $scope.productos = data;
-    });
-  }
-
-  $scope.obtenerProductosCarro();
-
-
-}).controller("seguimientoCtrl", function($http, $scope,$ionicModal,$location){
-  $scope.verificarToken();
-
-  $scope.seguimientos = {}
-
-  $scope.obtenerSeguimientos = function(){
-      $scope.verificarToken();
-
-    var res = $http.post($scope.dominio + '/usuario/seguimiento/ObtenerTodosLosSeguimientos',$scope.token);
-    res.success(function (data, status, headers, config) {
-      $scope.seguimientos = data;
-      console.log($scope.seguimientos);
-      var valoraciones = 0;
-      var totalValoracion = 0;
-      for(var x = 0; x < $scope.seguimientos.length;x++){
-        if($scope.seguimientos[x].productoValoracion.length === 0){
-          $scope.seguimientos[x].productoValoracion = totalValoracion;
-        }else{
-           for(var j = 0; j < $scope.seguimientos[x].productoValoracion.length;j++){
-             valoraciones = valoraciones + $scope.seguimientos[x].productoValoracion[j].valoracion;
-           }
-           totalValoracion = valoraciones / $scope.seguimientos[x].productoValoracion.length;
-           $scope.seguimientos[x].productoValoracion = totalValoracion;
-           totalValoracion = 0;
-           valoraciones = 0;
-        }
-      }
-
-      var index = 0;
-
-      for(var i = 0; i < $scope.seguimientos.length ;i++){
-        var marcas = $http.get($scope.dominio + '/marca/obtenerMarcaPorId?id='+$scope.seguimientos[i].idMarca);
-        marcas.success(function (data, status, headers, config) {
-          $scope.seguimientos[index].idMarca = data;
-          index++;
-        });
-      }
-
-    });
-  }
-
-$scope.obtenerSeguimientos();
-
-
-});
