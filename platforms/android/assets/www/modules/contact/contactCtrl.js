a4cc1d327ef2dcff019c3c3f6f61146d95c831b6
danished <danics92@gmail.com>
2017-06-13 18:57:56 +0200

    deleted trash

--- platforms/android/assets/www/modules/contact/contactCtrl.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by danilig on 15/05/17.
- */
-app.controller("contactoCtrl", function ($http, $scope, $location) {});
--- platforms/browser/www/modules/filterProducts/filterProductsCtrl.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by danilig on 15/05/17.
- */
-app.controller("filtradoCtrl", function ($http, $scope, $location) {});
